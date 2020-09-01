21aec83ef3f3b433ad0ac9d5a2c6cd0519c18172
Bryce Mardon <brycemardon@Bryces-MBP.modem>
2020-09-01 21:36:02 +1000

    Updates

---
+++ precache-manifest.e374869a1c722d2969111fa539656af7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e310588a27b355d289c90dcc3e39d3",
+    "url": "/%7BSmart-Brain%7D/index.html"
+  },
+  {
+    "revision": "f02261f944f5c2e94d79",
+    "url": "/%7BSmart-Brain%7D/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cd59a526a4f59209d854",
+    "url": "/%7BSmart-Brain%7D/static/css/main.616efd0c.chunk.css"
+  },
+  {
+    "revision": "f02261f944f5c2e94d79",
+    "url": "/%7BSmart-Brain%7D/static/js/2.fb3148a5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/%7BSmart-Brain%7D/static/js/2.fb3148a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd59a526a4f59209d854",
+    "url": "/%7BSmart-Brain%7D/static/js/main.690de4d4.chunk.js"
+  },
+  {
+    "revision": "f7c90b03ef4c16fb950e",
+    "url": "/%7BSmart-Brain%7D/static/js/runtime-main.8935ee61.js"
+  }
+]);
